856ce2a03c1ef4d1feef915797f3253df310101b
Jordan Nelson <jnelsontn@gmail.com>
2017-05-02 09:37:03 -0500

    removed invoice table join

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -1,4 +1,4 @@
 /* Provide a query that shows the top 5 most purchased tracks over all. */
 SELECT Track.Name 'Track Name', count(InvoiceLine.TrackId) AS TimesPurchased
-FROM InvoiceLine JOIN Track USING (TrackId) JOIN Invoice USING (InvoiceId)
+FROM InvoiceLine JOIN Track USING (TrackId)
 GROUP BY Track.Name ORDER BY TimesPurchased DESC LIMIT 5
